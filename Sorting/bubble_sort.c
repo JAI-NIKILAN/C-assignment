8367c060b6297c72ff28823e337967b28017e288
JAI-NIKILAN <jai77nikilan@gmail.com>
2025-07-24 00:37:25 +0530

    Update bubble_sort.c

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -10,9 +10,9 @@ int main()
 		printf("%d ",a[i]);
 	printf("\n");
 
-	for(i=0;i<n-1;i++)
+	for(i=0;i<n-1;i++)  //(n-1) because of a[j+1] in last with will count garbage
 	{
-		for(j=0;j<n-i-1;j++)
+		for(j=0;j<n-i-1;j++) // for using i because this is bubble sort value will move to last and settled so it will remains.In another iteration can't disturb
 		{
 			if(a[j]>a[j+1])
 			{
